caa884120340362a30ccfa8d05ee1aaff3dfa3dd
“victorpazb” <victorpazb@gmail.com>
2024-05-12 16:22:36 -0300

    chore: add node modules and api-service

---
+++ app-fimusgame/utils/apiService.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL: 'https://localhost:3000', // URL base da sua API
+  headers: {
+    'Content-Type': 'application/json',
+    // Adicione cabeçalhos personalizados aqui, se necessário
+  },
+});
+
+export default {
+  // Exemplo de função para buscar dados da API
+  async fetchData() {
+    try {
+      const response = await apiClient.get('/api/v1'); // Caminho do endpoint da API
+      return response.data; // Retorna os dados recebidos da API
+    } catch (error) {
+      console.error('Erro ao buscar dados da API:', error);
+      throw error; // Lança o erro para que seja tratado onde esta função for utilizada
+    }
+  },
+  
+  // Outras funções para interagir com sua API podem ser adicionadas aqui
+};
+
